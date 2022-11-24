cebd4f5eaf1f4a07b1da15ecaf5a4a9fa8fced07
burakcersit <burakcersit1@gmail.com>
2022-11-23 19:09:26 -0500

    completed js

---
+++ index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    inquirer
+
+        .prompt({
+            type: 'list',
+            name: 'begin choices',
+            message: 'What would you like to do? (Select on of the following)',
+            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Update Employee Role', 'View Departments', 'Add Department', 'View Roles', 'Add Role', 'View totalized budget', 'I am finished']
+        })
+        .then((data) => {
+            switch (data['begin choices']) {
+                case 'View All Employees':
+                    viewAllEmp();
+                    break;
+                case 'View All Employees By Department':
+                    viewEmpByDep();
+                    break;
+                case 'View All Employees By Manager':
+                    viewEmpByMngt();
+                    break;
+                case 'Add Employee':
+                    addEmp();
+                    break;
+                case 'Update Employee Role':
+                    upEmp();
+                    break;
+                case 'View Departments':
+                    viewDep();
+                    break;
+                case 'Add Department':
+                    addDep();
+                    break;
+                case 'View Roles':
+                    viewRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View totalized budget':
+                    addTotalByDep();
+                    break;
+                case 'I am finished':
+                    break;
+            }
+        })
+};
+
+module.exports = { promptUser }
+const { viewAllEmp, viewEmpByDep, viewEmpByMngt, addEmp, upEmp } = require('./lib/employee');
+const { viewDep, addDep } = require('./lib/department-methods');
+const { viewRoles, addRole } = require('./lib/roles-methods');
+const { addTotalByDep } = require('./lib/calculations');
+
+promptUser()
